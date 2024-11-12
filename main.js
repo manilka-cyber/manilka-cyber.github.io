ce9a5d8d9598a06c972e15b857b5ceeb5546fd05
Manilka Sooriyabandarae <mpsooriyabandara.com>
2024-11-12 10:47:34 +0530

    I add a file

--- main.js
+++ main.js
@@ -34,4 +34,30 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   });
-  
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".nav-menu li a");
+
+    window.addEventListener("scroll", () => {
+        let current = "";
+        
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            if (pageYOffset >= sectionTop - 50) {
+                current = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach((link) => {
+            link.classList.remove("active");
+            if (link.getAttribute("href").includes(current)) {
+                link.classList.add("active");
+            }
+        });
+    });
+});
+
+
+
+
